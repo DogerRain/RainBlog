8491c2208acc06422c45209049700b1689e1e9f2
huangyongwen <huangyongwen0306@163.com>
2019-11-28 00:34:59 +0800

    1. blog系统搭建

--- src/main/java/cn/yudianxx/system/controller/ArticleController.java
+++ src/main/java/cn/yudianxx/system/controller/ArticleController.java
@@ -12,8 +12,10 @@ import cn.yudianxx.system.response.ResponseResult;
 import cn.yudianxx.system.service.ArticleService;
 import cn.yudianxx.system.service.ArticleTagService;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
+import org.apache.tomcat.util.bcel.classfile.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,7 +25,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
@@ -131,7 +137,7 @@ public class ArticleController extends BaseController {
 
 
     @PostMapping(value = "/uploadFile")
-    public String uploadFile(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
+    public String uploadFile(@RequestParam(value = "editormd-image-file", required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
         ResponseResult result = new ResponseResult();
         Map<String, Object> map = new HashMap<String, Object>();
         File targetFile = null;
@@ -172,5 +178,38 @@ public class ArticleController extends BaseController {
         }
         return JSON.toJSONString(result);
     }
+    @RequestMapping("editormdPic")
+    @ResponseBody
+    public JSONObject editormdPic (@RequestParam(value = "editormd-image-file", required = true) MultipartFile file1, HttpServletRequest request,HttpServletResponse response) throws Exception {
+        String url = "G:\\源码\\Vue+Springboot\\src\\main\\resources\\static\\admin\\blog_image\\admin\\blog_image";
+        System.out.println(file1.getOriginalFilename());
+        File file = new File(url, file1.getOriginalFilename());
+        if (!file.exists() && file.mkdirs()) {
+        }
+        JSONObject resultJs = new JSONObject();
 
+        /*resultJs.put("success", "1");此处不要写 字符串的"1"，只是写为数字不要带引号*/
+        resultJs.put("success", 1);
+        resultJs.put("message", "上传成功");
+        resultJs.put("url", "http://localhost:8848/admin/lib/editormd/images/loading.gif");
+        return resultJs;
+    }
+
+    @RequestMapping("editormdPic1")
+    @ResponseBody
+    public JSONObject editormdPic1 (@RequestParam(value = "editormd-image-file", required = true) MultipartFile multipartFile, HttpServletRequest request,HttpServletResponse response) throws Exception {
+        String originalFilename = multipartFile.getOriginalFilename();
+        String extensionName = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String saveFileName =  Thread.currentThread().getId() + extensionName;
+        String a ="G:\\源码\\Vue+Springboot\\src\\main\\resources\\static\\admin\\blog_image\\";
+        Path path = Paths.get(a, saveFileName);
+        Files.write(path, multipartFile.getBytes());
+        multipartFile.transferTo(Paths.get(a, saveFileName).toFile());
+        String fileUrl = "http://localhost:8848" + "/admin/blog_image/" + saveFileName;
+        JSONObject result = new JSONObject();
+        result.put("success", 1);
+        result.put("message", "success");
+        result.put("url", fileUrl);
+        return result;
+    }
 }
