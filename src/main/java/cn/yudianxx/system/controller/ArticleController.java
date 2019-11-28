645b9da4bb9cc701b20b6fcfe9b305887ce0cb9a
huangyongwen <huangyongwen0306@163.com>
2019-11-28 14:08:54 +0800

    1. 富文本编辑器上传图片

--- src/main/java/cn/yudianxx/system/controller/ArticleController.java
+++ src/main/java/cn/yudianxx/system/controller/ArticleController.java
@@ -19,6 +19,7 @@ import org.apache.tomcat.util.bcel.classfile.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -136,8 +137,8 @@ public class ArticleController extends BaseController {
     }
 
 
-    @PostMapping(value = "/uploadFile")
-    public String uploadFile(@RequestParam(value = "editormd-image-file", required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/uploadFile")
+    public JSONObject uploadFile(@RequestParam(value = "editormd-image-file", required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
         ResponseResult result = new ResponseResult();
         Map<String, Object> map = new HashMap<String, Object>();
         File targetFile = null;
@@ -145,38 +146,35 @@ public class ArticleController extends BaseController {
         int code = 1;
         System.out.println(file);
         String fileName = file.getOriginalFilename();//获取文件名加后缀
+        JSONObject resultJs = new JSONObject();
         if (fileName != null && fileName != "") {
-            String returnUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath() + "/upload/imgs/";//存储路径
-            String path = request.getSession().getServletContext().getRealPath("upload/imgs"); //文件存储位置
+            String returnUrl =  "E:\\projet\\RainBlog\\src\\main\\resources\\static\\admin\\blog_image\\";
+            String path =returnUrl;
             String fileF = fileName.substring(fileName.lastIndexOf("."), fileName.length());//文件后缀
             fileName = new Date().getTime() + "_" + new Random().nextInt(1000) + fileF;//新的文件名
 
-            //先判断文件是否存在
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-            String fileAdd = sdf.format(new Date());
             //获取文件夹路径
-            File file1 = new File(path + "/" + fileAdd);
+            File file1 = new File(path + "/");
             //如果文件夹不存在则创建
             if (!file1.exists() && !file1.isDirectory()) {
                 file1.mkdir();
             }
             //将图片存入文件夹
             targetFile = new File(file1, fileName);
+
             try {
-                //将上传的文件写到服务器上指定的文件。
+                // 保存文件
                 file.transferTo(targetFile);
-                url = returnUrl + fileAdd + "/" + fileName;
-                code = 0;
-                result.setCode(code);
-                result.setMessage("图片上传成功");
-                map.put("url", url);
-                result.setUrl(url);
+                url = returnUrl   + fileName;
+                resultJs.put("success", 1);
+                resultJs.put("message", "上传成功");
+                resultJs.put("url", url);
             } catch (Exception e) {
                 e.printStackTrace();
                 result.setMessage("系统异常，图片上传失败");
             }
         }
-        return JSON.toJSONString(result);
+        return resultJs;
     }
     @RequestMapping("editormdPic")
     @ResponseBody
