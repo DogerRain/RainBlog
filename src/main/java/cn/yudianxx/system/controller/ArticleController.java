6034d92d9299b5dca3d5be0bae791e0f5c005e8a
huangyongwen <huangyongwen0306@163.com>
2019-11-28 16:25:08 +0800

    1. 富文本编辑器上传图片终于成功了，我tm太难了

--- src/main/java/cn/yudianxx/system/controller/ArticleController.java
+++ src/main/java/cn/yudianxx/system/controller/ArticleController.java
@@ -15,10 +15,12 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.tomcat.util.bcel.classfile.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.ServletInputStream;
@@ -47,6 +49,9 @@ public class ArticleController extends BaseController {
 
     @Autowired
     private ArticleTagService articleTagService;
+
+    @Autowired
+    QiNiuController qiNiuController;
 
     /**
      * 查询文章总数量
@@ -139,46 +144,26 @@ public class ArticleController extends BaseController {
 
     @RequestMapping(value = "/uploadFile")
     public JSONObject uploadFile(@RequestParam(value = "editormd-image-file", required = false) MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
+        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+        R<Map> r = qiNiuController.upload(file, request);
         ResponseResult result = new ResponseResult();
-        Map<String, Object> map = new HashMap<String, Object>();
-        File targetFile = null;
-        String url = "";//返回存储路径
-        int code = 1;
-        System.out.println(file);
-        String fileName = file.getOriginalFilename();//获取文件名加后缀
         JSONObject resultJs = new JSONObject();
-        if (fileName != null && fileName != "") {
-            String returnUrl =  "E:\\projet\\RainBlog\\src\\main\\resources\\static\\admin\\blog_image\\";
-            String path =returnUrl;
-            String fileF = fileName.substring(fileName.lastIndexOf("."), fileName.length());//文件后缀
-            fileName = new Date().getTime() + "_" + new Random().nextInt(1000) + fileF;//新的文件名
 
-            //获取文件夹路径
-            File file1 = new File(path + "/");
-            //如果文件夹不存在则创建
-            if (!file1.exists() && !file1.isDirectory()) {
-                file1.mkdir();
-            }
-            //将图片存入文件夹
-            targetFile = new File(file1, fileName);
-
-            try {
-                // 保存文件
-                file.transferTo(targetFile);
-                url = returnUrl   + fileName;
-                resultJs.put("success", 1);
-                resultJs.put("message", "上传成功");
-                resultJs.put("url", url);
-            } catch (Exception e) {
-                e.printStackTrace();
-                result.setMessage("系统异常，图片上传失败");
-            }
+        try {
+            resultJs.put("success", 1);
+            resultJs.put("message", "上传成功");
+//                resultJs.put("url", url);
+            resultJs.put("url", "http://"+r.getData().get("url"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.setMessage("系统异常，图片上传失败");
         }
         return resultJs;
     }
+
     @RequestMapping("editormdPic")
     @ResponseBody
-    public JSONObject editormdPic (@RequestParam(value = "editormd-image-file", required = true) MultipartFile file1, HttpServletRequest request,HttpServletResponse response) throws Exception {
+    public JSONObject editormdPic(@RequestParam(value = "editormd-image-file", required = true) MultipartFile file1, HttpServletRequest request, HttpServletResponse response) throws Exception {
         String url = "G:\\源码\\Vue+Springboot\\src\\main\\resources\\static\\admin\\blog_image\\admin\\blog_image";
         System.out.println(file1.getOriginalFilename());
         File file = new File(url, file1.getOriginalFilename());
@@ -195,11 +180,11 @@ public class ArticleController extends BaseController {
 
     @RequestMapping("editormdPic1")
     @ResponseBody
-    public JSONObject editormdPic1 (@RequestParam(value = "editormd-image-file", required = true) MultipartFile multipartFile, HttpServletRequest request,HttpServletResponse response) throws Exception {
+    public JSONObject editormdPic1(@RequestParam(value = "editormd-image-file", required = true) MultipartFile multipartFile, HttpServletRequest request, HttpServletResponse response) throws Exception {
         String originalFilename = multipartFile.getOriginalFilename();
         String extensionName = originalFilename.substring(originalFilename.lastIndexOf("."));
-        String saveFileName =  Thread.currentThread().getId() + extensionName;
-        String a ="G:\\源码\\Vue+Springboot\\src\\main\\resources\\static\\admin\\blog_image\\";
+        String saveFileName = Thread.currentThread().getId() + extensionName;
+        String a = "G:\\源码\\Vue+Springboot\\src\\main\\resources\\static\\admin\\blog_image\\";
         Path path = Paths.get(a, saveFileName);
         Files.write(path, multipartFile.getBytes());
         multipartFile.transferTo(Paths.get(a, saveFileName).toFile());
---
+++ src/test/java/cn/yudianxx/admin/dto/ThreadUploadTest.java
@@ -0,0 +1,20 @@
+package cn.yudianxx.admin.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author huangyongwen
+ * @date 2019/11/28
+ * @Description
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreadUploadTest {
+    @Test
+    public void Test(){
+
+    }
+}
