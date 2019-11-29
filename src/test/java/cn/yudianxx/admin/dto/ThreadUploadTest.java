822245ebc68defffda484e6e2e009f9c1e68f63d
huangyongwen <huangyongwen0306@163.com>
2019-11-29 16:55:11 +0800

    1. 线程测试。

--- src/test/java/cn/yudianxx/admin/dto/ThreadUploadTest.java
+++ src/test/java/cn/yudianxx/admin/dto/ThreadUploadTest.java
@@ -14,7 +14,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ThreadUploadTest {
     @Test
-    public void Test(){
-
+    public void Test() throws InterruptedException {
+        long strat = System.currentTimeMillis();
+        System.out.println(111);
+        Thread.sleep(5*1001);
+        System.out.println(555);
+        long end = System.currentTimeMillis()-strat;
+        System.out.println(String.format("用时：%s",end));
     }
 }
